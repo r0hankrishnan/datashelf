d8ce08bf2c2a358de4c37712ac95ead5525f9617
r0hankrishnan <125333525+r0hankrishnan@users.noreply.github.com>
2026-03-19 12:29:51 -0400

    Rip
    ok I forgot to rebuild the dist and pushed anyways so now I have to build and then upload again I suck at this

--- datashelf/__init__.py
+++ datashelf/__init__.py
@@ -4,6 +4,6 @@ from .inspect import ls, show
 from .load import load
 from .checkout import checkout
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = ["init", "save", "ls", "show", "load", "checkout"]
