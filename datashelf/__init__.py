b1b152ce6217b355eec14aa05720505624d87e4e
r0hankrishnan <125333525+r0hankrishnan@users.noreply.github.com>
2026-05-19 01:37:11 -0400

    update semver

--- datashelf/__init__.py
+++ datashelf/__init__.py
@@ -4,6 +4,6 @@ from datashelf.inspect import ls, show
 from datashelf.load import load
 from datashelf.checkout import checkout
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __all__ = ["init", "save", "ls", "show", "load", "checkout"]
