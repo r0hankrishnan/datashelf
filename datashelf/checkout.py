4f8817e85e1118303fba1f1e28e6d75f7c094ab6
r0hankrishnan <125333525+r0hankrishnan@users.noreply.github.com>
2026-05-19 01:17:01 -0400

    Refactor: Adjusted to use refactored load(), changed arg names for consistency, removed prints

--- datashelf/checkout.py
+++ datashelf/checkout.py
@@ -3,12 +3,12 @@ from pathlib import Path
 from datashelf.load import load
 
 
-def checkout(lookup_key: str, dest: str | Path) -> Path:
+def checkout(lookup_key: str, dest_path: str | Path) -> Path:
     """Copy a stored artifact from the datashelf to a user-specified destination.
 
     Args:
         lookup_key (str): Dataset name, full hash, or unique hash prefix.
-        dest (str | Path): Destination file path to copy the artifact to.
+        dest_path (str | Path): Destination file path to copy the artifact to.
     Raises:
         TypeError: If the destination file does not have a .parquet suffix.
         FileExistsError: If the destination file already exists.
@@ -16,18 +16,17 @@ def checkout(lookup_key: str, dest: str | Path) -> Path:
     Returns:
         Path: The path to the copied artifact.
     """
-    src_path = load(lookup_key=lookup_key)
+    src_path = load(lookup_key = lookup_key)
 
-    dest_path: Path = Path(dest).resolve()
+    dest_path = Path(dest_path).resolve()
 
     if dest_path.suffix != ".parquet":
-        raise TypeError(f"{dest} is invalid. Make sure file has .parquet suffix.")
+        raise TypeError(f"{dest_path} is invalid. Make sure file has .parquet suffix.")
 
     if dest_path.exists():
-        raise FileExistsError(f"Destination already exists: {dest_path}")
+        raise FileExistsError(f"File already exists at: {dest_path}")
 
     dest_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.copy2(str(src_path), str(dest_path))
 
-    print(f"Checked out artifact to {dest_path}")
     return dest_path
